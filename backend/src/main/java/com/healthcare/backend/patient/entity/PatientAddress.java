eacb5ec5b9d20b2006110f8b2733ee847be5d480
sandeeparavindi <sandeeparavindi1022@gmail.com>
2026-07-11 00:01:43 +0530

    patient address set

--- backend/src/main/java/com/healthcare/backend/patient/entity/PatientAddress.java
+++ backend/src/main/java/com/healthcare/backend/patient/entity/PatientAddress.java
@@ -1,4 +1,30 @@
 package com.healthcare.backend.patient.entity;
 
-public class PatientAddress {
+
+import com.healthcare.backend.common.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name="patient_addresses")
+@Getter
+@Setter
+public class PatientAddress extends BaseEntity {
+
+    private String street;
+
+
+    private String city;
+
+
+    private String district;
+
+
+    private String postalCode;
+
+    @OneToOne
+    @JoinColumn(name="patient_id")
+    private Patient patient;
+
 }
