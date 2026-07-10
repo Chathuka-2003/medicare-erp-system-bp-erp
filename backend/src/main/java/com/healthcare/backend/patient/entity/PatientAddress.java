1afac3509a29b1ccbdcdd860d6e1321781bf8d7c
sandeeparavindi <sandeeparavindi1022@gmail.com>
2026-07-10 23:47:56 +0530

    implemented patient mapper

--- backend/src/main/java/com/healthcare/backend/patient/entity/PatientAddress.java
+++ backend/src/main/java/com/healthcare/backend/patient/entity/PatientAddress.java
@@ -1,32 +1,4 @@
 package com.healthcare.backend.patient.entity;
 
-
-import com.healthcare.backend.common.base.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
-
-
-@Entity
-@Table(name="patient_addresses")
-@Getter
-@Setter
-public class PatientAddress extends BaseEntity {
-
-
-    private String street;
-
-
-    private String city;
-
-
-    private String district;
-
-
-    private String postalCode;
-
-
-    @OneToOne
-    @JoinColumn(name="patient_id")
-    private Patient patient;
-
+public class PatientAddress {
 }
