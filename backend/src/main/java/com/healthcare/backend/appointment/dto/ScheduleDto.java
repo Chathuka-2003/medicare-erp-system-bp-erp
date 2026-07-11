5327a9321bd0498cfebe21ea189e2760d56e6a7a
R.W.A.K.H Rajasekara <kavishirajasekara@gmail.com>
2026-07-11 10:47:20 +0530

    add ScheduleDto

--- backend/src/main/java/com/healthcare/backend/appointment/dto/ScheduleDto.java
+++ backend/src/main/java/com/healthcare/backend/appointment/dto/ScheduleDto.java
@@ -1,6 +1,14 @@
 package com.healthcare.backend.appointment.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.UUID;
+
+@Getter
+@Setter
 public class ScheduleDto {
 
-}
+    private UUID id;
