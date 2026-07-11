a28019ea07e2ff74c3ad66bc9af3319772493d5f
R.W.A.K.H Rajasekara <kavishirajasekara@gmail.com>
2026-07-11 10:32:52 +0530

    add appoinmentRequestDto

--- backend/src/main/java/com/healthcare/backend/appointment/dto/AppointmentRequestDto.java
+++ backend/src/main/java/com/healthcare/backend/appointment/dto/AppointmentRequestDto.java
@@ -1,5 +1,17 @@
 package com.healthcare.backend.appointment.dto;
 
+import com.healthcare.backend.appointment.enums.AppointmentStatus;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
 public class AppointmentRequestDto {
 
-}
+    @NotNull(message = "Patient is required")
+    private UUID patientId;
