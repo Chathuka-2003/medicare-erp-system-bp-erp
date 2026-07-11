1bb8761872265d77f7483919639f64520d350da6
sssanandasiri <sssanandasiri@students.nsbm.ac.lk>
2026-07-11 15:10:09 +0530

    implement doctor controller

--- backend/src/main/java/com/healthcare/backend/staff/dto/DoctorResponseDto.java
+++ backend/src/main/java/com/healthcare/backend/staff/dto/DoctorResponseDto.java
@@ -1,14 +1,4 @@
 package com.healthcare.backend.staff.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 public class DoctorResponseDto {
-
 }
