6dfa82dff25c941c8a55329ecc53030badbfacbd
R.W.A.K.H Rajasekara <kavishirajasekara@gmail.com>
2026-07-11 10:40:25 +0530

    add AppoinmentResponseDto

--- backend/src/main/java/com/healthcare/backend/appointment/dto/AppointmentResponseDto.java
+++ backend/src/main/java/com/healthcare/backend/appointment/dto/AppointmentResponseDto.java
@@ -1,7 +1,24 @@
 package com.healthcare.backend.appointment.dto;
 
+import com.healthcare.backend.appointment.enums.AppointmentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AppointmentResponseDto {
 
-
-}
+    private UUID id;
+    private LocalDateTime appointmentDate;
+    private String reason;
+    private String notes;
+    private AppointmentStatus status;
